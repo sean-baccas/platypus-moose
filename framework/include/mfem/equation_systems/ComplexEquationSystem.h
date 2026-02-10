c375bb0ad88116efbf6b55578434dbd8d42a2816
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2026-02-10 15:07:20 +0000

    Post rebase fixes

--- framework/include/mfem/equation_systems/ComplexEquationSystem.h
+++ framework/include/mfem/equation_systems/ComplexEquationSystem.h
@@ -132,6 +132,10 @@ public:
 
   // Pointer to complex GridFunctions to enable updates during nonlinear iterations
   Moose::MFEM::ComplexGridFunctions * _complex_gfuncs;
+  
+  private:
+
+    friend class ComplexEquationSystemProblemOperator;
 };
 
 template <class FormType>
